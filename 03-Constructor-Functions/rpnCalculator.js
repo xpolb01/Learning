0a9c5ec53e5c7969791918830467ab6d76b5c8b4
xpolb1 <bogdan.polovko@gmail.com>
2016-10-05 13:44:56 -0400

    Finished the calculator ablication. All tests passed

--- 03-Constructor-Functions/rpnCalculator.js
+++ 03-Constructor-Functions/rpnCalculator.js
@@ -1,6 +1,40 @@
-var RPNCalculator = [];
+var RPNCalculator = function() {};
+var emptyArrayError = function(message) {
+    this.message = message;
+  };
+emptyArrayError = "rpnCalculator is empty";
+RPNCalculator.prototype = Object.create(Array.prototype);
 RPNCalculator.prototype.plus = function() {
-
+  if(this.length === 0) {
+    throw emptyArrayError;
+  }
+  this.push(this.pop(this[this.length - 2]) + this.pop(this[this.length - 1]));
 };
-RPNCalculator.prototype.minus = function() {};
-RPNCalculator.prototype.value = function()
+RPNCalculator.prototype.minus = function() {
+  if(this.length === 0) {
+    throw emptyArrayError;
+  }
+    let lastNum = this.pop();
+    let firstNum = this.pop();
+    this.push(firstNum - lastNum);
+};
+RPNCalculator.prototype.divide = function() {
+  if(this.length === 0) {
+    throw emptyArrayError;
+  }
+    let a1 = this.pop();
+    let a2 = this.pop();
+    this.push(a2 / a1);
+};
+RPNCalculator.prototype.times = function() {
+  if(this.length === 0) {
+    throw emptyArrayError;
+  }
+    this.push(this.pop() * this.pop());
+};
+RPNCalculator.prototype.value = function() {
+  if(this.length === 0) {
+    throw emptyArrayError;
+  }
+    return this[this.length - 1];
+};
