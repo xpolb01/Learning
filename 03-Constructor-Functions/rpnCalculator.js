c1679a0984dec1879059daa5356754351c8727be
xpolb1 <bogdan.polovko@gmail.com>
2016-10-09 13:59:29 -0400

    Used IIFE, applied DRY principle

--- 03-Constructor-Functions/rpnCalculator.js
+++ 03-Constructor-Functions/rpnCalculator.js
@@ -1,41 +1,39 @@
-var RPNCalculator = function() {};
-// Adding testing comment
-var emptyArrayError = function(message) {
-    this.message = message;
-  };
-emptyArrayError = "rpnCalculator is empty";
+var RPNCalculator = function() {
+  this.emptyArrayError = "rpnCalculator is empty";
+};
 RPNCalculator.prototype = Object.create(Array.prototype);
-RPNCalculator.prototype.plus = function() {
-  if(this.length === 0) {
-    throw emptyArrayError;
-  }
-  this.push(this.pop(this[this.length - 2]) + this.pop(this[this.length - 1]));
-};
-RPNCalculator.prototype.minus = function() {
-  if(this.length === 0) {
-    throw emptyArrayError;
-  }
-    let lastNum = this.pop();
-    let firstNum = this.pop();
-    this.push(firstNum - lastNum);
-};
-RPNCalculator.prototype.divide = function() {
-  if(this.length === 0) {
-    throw emptyArrayError;
-  }
-    let a1 = this.pop();
-    let a2 = this.pop();
-    this.push(a2 / a1);
-};
-RPNCalculator.prototype.times = function() {
-  if(this.length === 0) {
-    throw emptyArrayError;
-  }
-    this.push(this.pop() * this.pop());
-};
-RPNCalculator.prototype.value = function() {
-  if(this.length === 0) {
-    throw emptyArrayError;
-  }
-    return this[this.length - 1];
-};
+(function(rpnProt) {
+  rpnProt.popCompute = function(operationFunc) {
+    if(this.length < 2) {
+      throw this.emptyArrayError;
+    }
+    else {
+      let num1 = this.pop();
+      let num2 = this.pop();
+      this.push(operationFunc(num1,num2));
+    }
+  };
+  rpnProt.plus = function() {
+      this.popCompute(function(first, second) {
+        return first + second;
+      });
+  };
+  rpnProt.minus = function() {
+    this.popCompute(function(first, second) {
+        return second - first;
+    });
+  };
+  rpnProt.divide = function() {
+    this.popCompute(function(first, second) {
+        return second / first;
+    });
+  };
+  rpnProt.times = function() {
+    this.popCompute(function(first, second) {
+        return second * first;
+    });
+  };
+  rpnProt.value = function() {
+      return this[this.length - 1];
+  };
+})(RPNCalculator.prototype);
