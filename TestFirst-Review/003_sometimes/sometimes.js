59ad33bac4f10833c9b4c5488a4e6baf4f62678b
xpolb1 <bogdan.polovko@gmail.com>
2016-10-24 15:23:41 -0400

    Added some changes

--- TestFirst-Review/003_sometimes/sometimes.js
+++ TestFirst-Review/003_sometimes/sometimes.js
@@ -1,6 +1,5 @@
 var sometimes = function (func) {
   var timesCalled = 1;
-  var arr = [];
   return function(a, b) {
     if(timesCalled < 4 ? true : timesCalled % 2 == 0 ? false : true ) {
       timesCalled++;
