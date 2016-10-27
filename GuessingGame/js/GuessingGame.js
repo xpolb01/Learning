b17bfc883e91718a4b40fd22e02133b5565e465d
xpolb1 <bogdan.polovko@gmail.com>
2016-10-27 12:43:20 -0400

    Finished the game

--- GuessingGame/js/GuessingGame.js
+++ GuessingGame/js/GuessingGame.js
@@ -40,23 +40,33 @@ Game.prototype.playersGuessSubmission = function(num) {
 };
 
 Game.prototype.checkGuess = function() {
-  if(this.playersGuess === this.winningNumber) {
+  var diff = this.difference();
+  var guesses = this.pastGuesses;
+  var guess = this.playersGuess;
+  if(this.isLower()) {
+    $('#subtitle').text("Guess Higher!")
+  } else {
+    $('#subtitle').text("Guess Lower!")
+  }
+  if(guess === this.winningNumber) {
     var win = "You Win!";
     return win;
   }
-  else if(this.pastGuesses.indexOf(this.playersGuess) >= 0) {
+  else if(guesses.indexOf(guess) >= 0) {
     var again ='You have already guessed that number.';
     return again;
   }
   else {
-    this.pastGuesses.push(this.playersGuess);
-    if(this.pastGuesses.length >= 5) {
+    guesses.push(guess);
+    $('#guess-list li:nth-child('+ guesses.length +')').text(guess);
+    if(guesses.length >= 5) {
+      $('#subtitle').text("Press the Reset button to play again!")
       return "You Lose."
     }
-    if(this.difference() <= 100) {
-      if(this.difference() < 50){
-        if(this.difference() < 25) {
-          if(this.difference() < 10) {
+    if(diff <= 100) {
+      if(diff < 50){
+        if(diff < 25) {
+          if(diff < 10) {
             return "You're burning up!";
           }
           return "You're lukewarm.";
@@ -76,7 +86,26 @@ Game.prototype.provideHint = function() {
   return shuffle([generateWinningNumber(),generateWinningNumber(), this.winningNumber]);
 };
 
-
+$(document).ready(function() { 
+  var createGame = newGame();
+  $('#submit').on('click', function() {
+    if(createGame.pastGuesses.length < 5) {
+      var bla = Number($('#players-input').val());
+      $('#players-input').val('');
+      var result = createGame.playersGuessSubmission(bla);
+      $('#title').text(result);
+    }
+  });
+  $('#reset').on('click', function() {
+    $('#title').text('Play the Guessing Game!');
+    $('#subtitle').text('Guess a number between 1-100!')
+    $('.guess').text('-');
+  });
+  $('#hint').click(function() {
+    var hints = createGame.provideHint();
+    $('#title').text('The winning number is '+hints[0]+', '+hints[1]+', or '+hints[2]);
+  });
+});
 
 
 
